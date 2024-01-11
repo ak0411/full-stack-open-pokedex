536e91e389f6631f9b282c8ada0a0786e2e5af9d
Anawil Karavek <akaravek@hotmail.com>
2024-01-11 18:08:56 +0100

    fix lint errors

--- app.js
+++ app.js
@@ -6,7 +6,6 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
-console.log(PORT);
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
