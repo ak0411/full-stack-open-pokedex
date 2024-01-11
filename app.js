2b9eca13923dea310371c7711b93e968d1dee55a
Anawil Karavek <akaravek@hotmail.com>
2024-01-11 18:05:23 +0100

    added e2e test

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const PORT = process.env.PORT || 5000
 
 app.use(express.static('dist'))
 
+console.log(PORT);
 app.listen(PORT, () => {
   // eslint-disable-next-line no-console
   console.log('server started on port 5000')
